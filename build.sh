4533e572837a788da555d7705dfef94c36e1182b
Jakob <22324536+jakobttu@users.noreply.github.com>
2019-09-23 22:15:14 +0300

    #108 | remove build script file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CLONED_JAVADOC_LOCATION="materjalid"
-rm -rf $CLONED_JAVADOC_LOCATION
-
-git clone https://github.com/taltech-coding/javadoc.git $CLONED_JAVADOC_LOCATION
-
-source ./materjalid/setup.sh
